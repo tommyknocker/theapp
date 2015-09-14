b154254f73fde05d986bc626f29b54d83925270d
tommyknocker <ulin.vasiliy@gmail.com>
2015-09-14 21:04:51 +0300

    handlers now are in the \App\Handlers namespace.

--- handlers/System.handler.php
+++ handlers/System.handler.php
@@ -1,10 +1,14 @@
 <?php
+
+namespace App\Handlers;
+use \App;
+
 /**
  * The App.Pro site class
  *
  * @author Tommyknocker <tommyknocker@theapp.pro>
  */
-class HSystem
+class System
 {
 
     /**
@@ -12,7 +16,7 @@ class HSystem
      */
     public static function init()
     {
-        App::Event()->register('system:errors', 'showErrors');
+        App::Event()->subscribe('system:errors', 'showErrors');
     }
 
     /**
@@ -26,5 +30,5 @@ class HSystem
                 echo EOL . '[' . $error['date_create'] . '] ' . $container->errorsTranslation[$error['type']] . ' in file ' . $error['file'] . ' on line ' . $error['line'] . ': ' . $error['name'] . ', ' . $error['variable'];
             }
         }
-    }
+    }    
 }
