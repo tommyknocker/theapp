0e665d15602eb838374e507d6bff08e8eb69ff44
tommyknocker <ulin.vasiliy@gmail.com>
2015-09-22 02:40:42 +0300

    TNoSingleton trait, that informs App to reinit object on every __callStatic

---
+++ traits/TNoSingleton.trait.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Dumb trait that informs App not to reinit object on each App::Object() call
+ *
+ * @author Tommyknocker <tommyknocker@theapp.pro>
+ * @license http://www.gnu.org/licenses/lgpl.txt LGPLv3
+ */
+trait TNoSingleton
+{
+    
+}
