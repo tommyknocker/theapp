1103b83fcbb5a5190449697a08e6611f10f836f1
tommyknocker <ulin.vasiliy@gmail.com>
2015-09-22 02:41:20 +0300

    Direct handler execution App::Handler('HandlerName')->HandlerMethod();

---
+++ core/Handler.class.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Direct handler execution
+ * 
+ * App::Handler('HandlerName')->HandlerMethod();
+ *
+ * @author Tommyknocker <tommyknocker@theapp.pro>
+ * @license http://www.gnu.org/licenses/lgpl.txt LGPLv3
+ */
+namespace App\Core;
+
+use App\Handlers;
+
+class Handler
+{
+
+    use \TCallable,
+        \TNoSingleton;
+
+    /**
+     * Handler object
+     * @var object 
+     */
+    private $handlerObject = null;
+
+    public function __construct($handlerName)
+    {
+        $handlerName = '\\App\\Handlers\\' . $handlerName;
+        $this->handlerObject = new $handlerName();
+    }
+
+    /**
+     * Call handler method
+     * @param string $method
+     * @param array $arguments
+     * @return mixed
+     */
+    public function __call($method, $arguments)
+    {
+        return call_user_func_array([$this->handlerObject, $method], $arguments);
+    }
+}
