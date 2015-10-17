db36f104ef544e0713ad999500d039ca714a7fbc
tommyknocker <ulin.vasiliy@gmail.com>
2015-10-17 12:36:01 +0300

    unset() overloading. methods alphabetical sorting

--- core/Container.class.php
+++ core/Container.class.php
@@ -17,19 +17,63 @@ class Container
     private $storage = [];
 
     /**
-     * Add value to a container array
+     * Get data from container
+     * @param string $name Variable name
+     * @return mixed Data
+     */
+    public function __get($name)
+    {
+        return array_key_exists($name, $this->storage) ? $this->storage[$name] : null;
+    }
+
+    /**
+     * isset() overloading
+     *
+     * @param  string $name
+     * @return bool
+     */
+    public function __isset($name)
+    {
+        return isset($this->storage[$name]);
+    }
+
+    /**
+     * Put data to container
+     * @param string $name Variable name
+     * @param mixed $value Value
+     */
+    public function __set($name, $value)
+    {
+        $this->storage[$name] = $value;
+    }
+
+    /**
+     * unset() overloading
+     *
+     * @param  string $name
+     * @return void
+     */
+    public function __unset($name)
+    {
+        if (isset($this->storage[$name])) {
+            unset($this->storage[$name]);
+        }
+    }
+
+    /**
+     * Append value to a container array
      * @param string $name
      * @param mixed $value
      */
-    public function add($name, $value)
+    public function append($name, $value)
     {
         if (!array_key_exists($name, $this->storage)) {
             $this->storage[$name] = [];
         }
 
         $this->storage[$name][] = $value;
-    }
-
+    }    
+    
     /**
      * Get data by pattern
      * @param string $pattern Valid PCRE pattern
@@ -46,27 +90,5 @@ class Container
         }
 
         return $result;
-    }
-
-    /**
-     * Put data to container
-     * @param string $name Variable name
-     * @param mixed $value Value
-     */
-    public function __set($name, $value)
-    {
-        $this->storage[$name] = $value;
-        \App::removeFromStack();
-    }
-
-    /**
-     * Get data from container
-     * @param string $name Variable name
-     * @return mixed Data
-     */
-    public function __get($name)
-    {
-        \App::removeFromStack();
-        return array_key_exists($name, $this->storage) ? $this->storage[$name] : null;
     }
 }
