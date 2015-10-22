c0e0300fe21dfd055d72fc0b1d8df0d8c5447ec4
tommyknocker <ulin.vasiliy@gmail.com>
2015-10-22 23:39:26 +0300

    TGetter trait Format::convertCamelCaseToUnderScore

--- core/Format.class.php
+++ core/Format.class.php
@@ -91,4 +91,19 @@ class Format
 
         return $result;
     }
+    
+    /**
+     * Convert camel case to underscore
+     * @param string $string
+     * @return string
+     */
+    public function camelCaseToUnderScore($string)
+    {
+        preg_match_all('/([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)/', $string, $matches);
+        $result = $matches[0];
+        foreach ($result as &$match) {
+            $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
+        }
+        return implode('_', $result);
+    }    
 }
---
+++ traits/TGetter.trait.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Impletent smart magic getting
+ *
+ * @author Tommyknocker <tommyknocker@theapp.pro>
+ * @license http://www.gnu.org/licenses/lgpl.txt LGPLv3
+ */
+trait TGetter
+{
+    use TCallable;
+    /**
+     * Name of property, that contains all the stuff
+     * @var string 
+     */
+    private $dataFieldName = 'data';
+
+    public function __call($method, $args)
+    {        
+        if(substr($method, 0, 3) === 'get') {        
+     
+            if(!isset($this->{$this->dataFieldName}) || !is_array($this->{$this->dataFieldName})) {
+                App::Log()->addWarning('No such field or field is not an array');
+                return null;
+            }
+                        
+            $method = App::Format()->camelCaseToUnderScore(substr($method, 3))->result;
+            
+            return isset($this->{$this->dataFieldName}[$method]) ? $this->{$this->dataFieldName}[$method] : null;
+        }        
+    }
+
+    /**
+     * Set the name of data stuff property
+     * @param string $name
+     */
+    public function setDataFieldName($name)
+    {
+        $this->dataFieldName = $name;
+    }
+}
