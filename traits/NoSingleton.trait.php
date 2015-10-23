caf88e847f9c5f0e1ceb9d74535b8ece81b3dc1e
tommyknocker <ulin.vasiliy@gmail.com>
2015-10-23 14:23:48 +0300

    fix typo

--- traits/NoSingleton.trait.php
+++ traits/NoSingleton.trait.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Dumb trait that informs App not to reinit object on each App::Object() call
+ * Dumb trait that informs App to reinit object on each App::Object() call
  *
  * @author Tommyknocker <tommyknocker@theapp.pro>
  * @license http://www.gnu.org/licenses/lgpl.txt LGPLv3
